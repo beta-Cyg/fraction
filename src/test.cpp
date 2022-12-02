f3bfefb0001b56d6dc1904bc12992d583a800947
beta-cyg <beta-Cyg@github.com>
2022-12-02 20:42:25 +0800

    change src/test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -2,15 +2,9 @@ import fraction;
 import <iostream>;
 
 frt::fraction a{};
-frt::fraction b{1l};
-frt::fraction c{1ll};
-frt::fraction d{1,1};
 
 auto main()->int{
-	std::cout<<a<<std::endl;
-	std::cout<<b<<std::endl;
-	std::cout<<c<<std::endl;
-	std::cout<<d<<std::endl;
+	std::cout<<typeid(a).name()<<std::endl;
 
 	return 0;
 }
