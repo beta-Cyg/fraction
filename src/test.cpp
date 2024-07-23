ae194764a98df95ebca5a5b025067e700274d690
cygnus-beta <Syz748748@163.com>
2024-07-23 12:23:29 +0800

    optimize part of reduce(); change numerator, denominator, reduce() to public; add move constructor;

--- src/test.cpp
+++ src/test.cpp
@@ -1,17 +1,26 @@
 import fraction;
 import <iostream>;
+import <concepts>;
 
 frt::fraction a{1};
-frt::fraction<int16_t> b{2};
+frt::fraction<long long> b{2};
+
+template<std::signed_integral Int1,std::signed_integral Int2>
+void swap(frt::fraction<Int1>& v1,frt::fraction<Int2>& v2){
+	v1+=v2;
+	v2=v1-v2;
+	v1-=v2;
+}
 
 auto main()->int{
 	std::cout<<(a+=2)<<std::endl
 		<<(a/=2)<<std::endl
 		<<(a*=3)<<std::endl
 		<<(a-=1)<<std::endl;
-	a=0;
-	a+=b;
-	std::cout<<frt::fraction{-1}<<std::endl;
+	std::cout<<a<<' '<<b<<std::endl;
+	swap(a,b);
+	std::cout<<a<<' '<<b<<std::endl;
+	std::cout<<frt::fraction{-1}+frt::fraction{1,2}<<std::endl;
 
-	return 0;
+	return double{frt::fraction{0}};
 }
--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,8 @@
-set_languages("c++20")
+set_toolchains("gcc-13")
+set_languages("c++23")
+add_rules("mode.release","mode.debug")
 
-target("test.out")
+target("test")
 	set_kind("binary")
-	add_files("src/*.mpp","src/*.cpp")
+	add_files("src/*.mpp","src/test.cpp")
+
